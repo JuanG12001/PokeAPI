b02d872a662799928d6f2dabba6f56af5466ef38
JuanDev <juanenriquegarcia126@gmail.com>
2024-10-29 16:48:36 -0400

    ajuste clase button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import "./Button.css";
 export const Button = (  )=>{
 
   return `
-    <button class="buttom buttom--animation">
+    <button class="buttom buttom--card buttom--animation">
       <img class="icon-pokeball" src="/src/assets/icon-pokeball.png" alt="icon-pokeball">
       Capturar Pokémon
     </button>
